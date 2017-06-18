b0d60466ff451e0f2f61bd24a78daa1c1dbae9ef
Filipe <granha.carlos@gmail.com>
2017-06-18 20:01:11 +0100

    added - if options id - to each initialize

--- models/tag.rb
+++ models/tag.rb
@@ -7,7 +7,7 @@ class Tag
   attr_reader(:id, :name)
 
   def initialize(options)
-    @id = options['id'].to_i
+    @id = options['id'].to_i if options ['id']
     @name = options['name']
   end
 
--- models/transaction.rb
+++ models/transaction.rb
@@ -7,7 +7,7 @@ class Transaction
   attr_reader(:id, :amount, :day, :merchant_id, :tag_id)
 
   def initialize(options)
-    @id = options["id"].to_i
+    @id = options["id"].to_i if options ['id']
     @amount = options["amount"]
     @day = options["day"]
     @merchant_id = options["merchant_id"].to_i
