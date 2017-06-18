2f2729e571d8c5ef2b130f26ffe785de136fed9b
Filipe <granha.carlos@gmail.com>
2017-06-18 16:54:44 +0100

    classes created in the models files

---
+++ models/tag.rb
@@ -0,0 +1,17 @@
+require_relative("../db/sql_runner")
+
+class Tag
+
+
+attr_reader(:name)
+
+def initialize(options)
+  @id = ["id"].to_i
+  @name = ["name"]
+end
+
+
+
+
+
+end
---
+++ models/transaction.rb
@@ -0,0 +1,21 @@
+require_relative("../db/sql_runner")
+
+class Transaction
+
+
+attr_reader(:name)
+
+def initialize(options)
+  @id = ["id"].to_i
+  @amount = ["amount"]
+  @name = ["name"]
+  @date = ["date"]
+  @merchant_id = ["merchant_id"].to_i
+  @tag_id = ["tag_id"].to_i
+end
+
+
+
+
+
+end
